794232fef05ac484d7067b7887ea09190ca771bf
jorpaz <giovanny.paz@outlook.com>
2023-03-14 23:06:57 -0600

    api fetch config

--- src/hooks/useGetProducts.js
+++ src/hooks/useGetProducts.js
@@ -2,14 +2,22 @@ import { useEffect, useState } from "react";
 import axios from 'axios';
 
 const useGetProducts = (API) => {
-	const [products, setProducts] = useState([]);
+    const [products, setProducts] = useState([]);
 
-	useEffect(async () => {
-		const response = await axios(API);
-		setProducts(response.data);
-	}, []);
+    useEffect(() => {
+        async function fetchProducts() { // Se define una función async
+            try {
+                const response = await axios(API);
+                setProducts(response.data);
+            } catch (error) {
+                console.error(error);
+            }
+        }
 
-	return products;
+        fetchProducts(); // Se llama a la función async
+    }, [API]);
+
+    return products;
 };
 
 export default useGetProducts;
